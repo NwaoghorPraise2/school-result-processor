165f416853a6a0c59a936a1b944c0fef1d2e9e71
Nwaoghor Praise Chukunweiken <nwaoghorpraise@admins-air.home>
2024-03-28 22:35:28 +0000

    Initaiilizing Project

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, {Application, Request, Response} from 'express';
+
+const app: Application = express();
+const port: number = 3000;
+
+app.get('/', (req:Request, res:Response) => {
+    res.send("Testing");
+});
+
+
+app.listen(port, () => {
+    console.log(`Connected`);
+});
