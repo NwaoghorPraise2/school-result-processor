1218c1349d24c120c8add7cca729d7a6731aa8a5
Nwaoghor Praise Chukunweiken <nwaoghorpraise@admins-air.home>
2024-03-30 20:34:41 +0000

    Project Setup Cont'd

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hi");
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.json({message: "Hi. testing things"});
+});
+
+const port = Number(process.env.PORT || 3000);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
